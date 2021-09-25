8317ab8ce71595e9cab23b26ee98481b8fa47997
Yagoor <yago.rosario@hotmail.com.br>
2021-09-25 11:04:31 +0000

    Added profile test

---
+++ tests/profile/basic/basic.c
@@ -0,0 +1,181 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Yago Fontoura do Rosário <yago.rosario@hotmail.com.br>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <string.h>
+
+#include "hash_table.h"
+#include "hash_table_iterator.h"
+
+typedef struct {
+  uint32_t key;
+} basic_key_t;
+
+typedef struct {
+  uint32_t      x;
+  uint32_t      y;
+} basic_data_t;
+
+#define BASIC_HASH_ENTRIES_SIZE    10
+
+typedef struct {
+  hash_table_t  hash_table;
+  uint8_t       data[(sizeof(hash_table_entry_t) + sizeof(basic_key_t) +
+      sizeof(basic_data_t)) * BASIC_HASH_ENTRIES_SIZE];
+} basic_hash_table_t;
+
+uint32_t basic_hash_function(uint8_t *key)
+{
+  basic_key_t *basic_key;
+
+  basic_key = (basic_key_t *)key;
+
+  return ((basic_key->key * 32) >> 8);
+}
+
+
+int main(void)
+{
+  uint32_t i;
+  basic_key_t basic_key;
+  basic_data_t basic_data;
+  basic_hash_table_t basic_hash;
+  hash_table_iterator_t hash_table_iterator;
+  uint8_t key_checker[BASIC_HASH_ENTRIES_SIZE];
+
+  hash_table_init((hash_table_t *)&basic_hash,
+      (hash_function_t)basic_hash_function,
+      BASIC_HASH_ENTRIES_SIZE, sizeof(basic_data_t), sizeof(basic_key_t));
+
+  /* Populate hash_table ensuring that repeated keys is not allowed */
+  for (i = 1; i <= BASIC_HASH_ENTRIES_SIZE; i++)
+  {
+    basic_key.key = i;
+    basic_data.x = i;
+    basic_data.y = BASIC_HASH_ENTRIES_SIZE - i;
+    if (!(hash_table_insert((hash_table_t *)&basic_hash,
+        (uint8_t *)&basic_key,
+        (uint8_t *)&basic_data))) {
+      return (1);
+    }
+
+    /* Check hash_table count */
+    if (!(hash_table_count((hash_table_t *)&basic_hash) == i)) {
+      return (1);
+    }
+  }
+
+  memset(key_checker, 0, sizeof(key_checker));
+  hash_table_iterator_init(&hash_table_iterator, (hash_table_t *)&basic_hash);
+
+  while (hash_table_iterator_get_next(&hash_table_iterator,
+      (hash_table_t *)&basic_hash, (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data))
+  {
+    if (!(key_checker[basic_key.key - 1] == 0)) {
+      return (1);
+    }
+    if (!(basic_data.x + basic_data.y == 10)) {
+      return (1);
+    }
+    key_checker[basic_key.key - 1] = 1;
+  }
+
+  /* Verify key checker */
+  for (i = 0; i < BASIC_HASH_ENTRIES_SIZE; i++)
+  {
+    if (!(key_checker[i] == 1)) {
+      return (1);
+    }
+  }
+
+  /* Try to insert when hash_table is full */
+  basic_key.key = 20;
+  if (hash_table_insert((hash_table_t *)&basic_hash,
+      (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data)) {
+    return (1);
+  }
+
+  /* Remove one item */
+  basic_key.key = 9;
+  if (!(hash_table_remove((hash_table_t *)&basic_hash,
+      (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data))) {
+    return (1);
+  }
+  if (!(basic_data.x == 9)) {
+    return (1);
+  }
+  if (!(basic_data.y == 1)) {
+    return (1);
+  }
+
+  /* Try to remove it again */
+  basic_key.key = 9;
+  if (hash_table_remove((hash_table_t *)&basic_hash,
+      (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data)) {
+    return (1);
+  }
+
+  /* Try to remove item that was not added */
+  basic_key.key = 20;
+  if (hash_table_remove((hash_table_t *)&basic_hash,
+      (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data)) {
+    return (1);
+  }
+
+  /* Get item from hash_table and check content */
+  basic_key.key = 4;
+  if (!(hash_table_get((hash_table_t *)&basic_hash, (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data))) {
+    return (1);
+  }
+  if (!(basic_data.x == 4)) {
+    return (1);
+  }
+  if (!(basic_data.y == 6)) {
+    return (1);
+  }
+
+  /* Remove same item from hash_table */
+  basic_key.key = 4;
+  if (!(hash_table_remove((hash_table_t *)&basic_hash,
+      (uint8_t *)&basic_key,
+      NULL))) {
+    return (1);
+  }
+
+  /* Try to get it again */
+  basic_key.key = 4;
+  if (hash_table_get((hash_table_t *)&basic_hash,
+      (uint8_t *)&basic_key,
+      (uint8_t *)&basic_data)) {
+    return (1);
+  }
+
+  return (0);
+}
---
+++ tests/profile/profile.mk
@@ -0,0 +1,68 @@
+## MIT License
+## 
+## Copyright (c) 2021 Yago Fontoura do Rosário <yago.rosario@hotmail.com.br>
+## 
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+## 
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+## 
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+##
+
+NM = nm
+
+SOURCE_DIRS += $(BUILDROOT)/hash_table/src
+C_FLAGS += -I$(BUILDROOT)/hash_table/include
+SOURCE_C += hash_table.c hash_table_iterator.c
+
+# C source files
+SOURCE_C += $(TEST_C)
+
+# Targets
+TARGETS = $(BUILD_DIR)/$(TEST)
+
+include $(BUILDROOT)/base.mk
+
+# Extra rules at the end
+
+profile: $(TARGETS)
+	@echo "--- Profiling $(TEST)"
+	$(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4
+
+run: profile
+
+check: $(TARGETS)
+	@echo "--- Checking $(TEST)"
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | wc -l}" != "6" ]; \
+		then echo "Failed to check length of symbols" && exit 1; \
+	fi
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | grep "hash_table_iterator_init" | cut -d ' ' -f1,1}" != "0000000000000016" ]; \
+		then echo "Failed to check size of hash_table_iterator_init" && exit 1; \
+	fi
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | grep "hash_table_init" | cut -d ' ' -f1,1}" != "0000000000000062" ]; \
+		then echo "Failed to check size of hash_table_init" && exit 1; \
+	fi
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | grep "hash_table_iterator_get_next" | cut -d ' ' -f1,1}" != "0000000000000165" ]; \
+		then echo "Failed to check size of hash_table_iterator_get_next" && exit 1; \
+	fi
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | grep "hash_table_get" | cut -d ' ' -f1,1}" != "0000000000000203" ]; \
+		then echo "Failed to check size of hash_table_get" && exit 1; \
+	fi
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | grep "hash_table_insert" | cut -d ' ' -f1,1}" != "0000000000000217" ]; \
+		then echo "Failed to check size of hash_table_insert" && exit 1; \
+	fi
+	@if [ "${shell $(NM) -S -td --size-sort $< | grep -i " [t] " | grep "hash_table" | cut -d ' ' -f2,4 | grep "hash_table_remove" | cut -d ' ' -f1,1}" != "0000000000000287" ]; \
+		then echo "Failed to check size of hash_table_remove" && exit 1; \
+	fi
