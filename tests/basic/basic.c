8526af45a0431f4a0f8ca7ca2b6bdb5e4117f18b
Yago Fontoura do Rosário <yago.rosario@hotmail.com.br>
2021-10-02 11:20:29 +0200

    Data as external pointer (#26)
    * Data as external pointer
    
    * Code-style fix

--- tests/basic/basic.c
+++ tests/basic/basic.c
@@ -44,11 +44,9 @@ typedef struct {
 
 #define BASIC_HASH_ENTRIES_SIZE    10
 
-typedef struct {
-  ht_t          hash_table;
-  uint8_t       data[(sizeof(ht_entry_t) + sizeof(basic_key_t) +
-      sizeof(basic_data_t)) * BASIC_HASH_ENTRIES_SIZE];
-} basic_ht_t;
+static ht_t hash_table;
+static uint8_t hash_table_data[((sizeof(ht_entry_t) +sizeof(basic_key_t) +
+    sizeof(basic_data_t)) * BASIC_HASH_ENTRIES_SIZE)];
 
 static uint32_t basic_hash_function(uint8_t *key)
 {
@@ -60,23 +58,22 @@ static uint32_t basic_hash_function(uint8_t *key)
 }
 
 
-void test_basic_hash(void **state)
+void test_hash(void **state)
 {
-  basic_ht_t basic_hash;
+  (void)state;
+
   basic_key_t basic_key;
   basic_data_t basic_data;
 
-  basic_hash = (*(basic_ht_t *)(*state));
-
   /* Try to insert when hash_table is full */
   basic_key.key = 20;
-  assert_false(ht_insert((ht_t *)&basic_hash,
+  assert_false(ht_insert(&hash_table,
       (uint8_t *)&basic_key,
       (uint8_t *)&basic_data));
 
   /* Remove one item */
   basic_key.key = 9;
-  assert_true(ht_remove((ht_t *)&basic_hash,
+  assert_true(ht_remove(&hash_table,
       (uint8_t *)&basic_key,
       (uint8_t *)&basic_data));
   assert_true(basic_data.x == 9);
@@ -84,53 +81,52 @@ void test_basic_hash(void **state)
 
   /* Try to remove it again */
   basic_key.key = 9;
-  assert_false(ht_remove((ht_t *)&basic_hash,
+  assert_false(ht_remove(&hash_table,
       (uint8_t *)&basic_key,
       (uint8_t *)&basic_data));
 
   /* Try to remove item that was not added */
   basic_key.key = 20;
-  assert_false(ht_remove((ht_t *)&basic_hash,
+  assert_false(ht_remove(&hash_table,
       (uint8_t *)&basic_key,
       (uint8_t *)&basic_data));
 
   /* Get item from hash_table and check content */
   basic_key.key = 4;
-  assert_true(ht_get((ht_t *)&basic_hash, (uint8_t *)&basic_key,
+  assert_true(ht_get(&hash_table, (uint8_t *)&basic_key,
       (uint8_t *)&basic_data));
   assert_true(basic_data.x == 4);
   assert_true(basic_data.y == 6);
 
   /* Remove same item from hash_table */
   basic_key.key = 4;
-  assert_true(ht_remove((ht_t *)&basic_hash,
+  assert_true(ht_remove(&hash_table,
       (uint8_t *)&basic_key,
       NULL));
 
   /* Try to get it again */
   basic_key.key = 4;
-  assert_false(ht_get((ht_t *)&basic_hash,
+  assert_false(ht_get(&hash_table,
       (uint8_t *)&basic_key,
       (uint8_t *)&basic_data));
 }
 
 
-void test_basic_hash_iterator(void **state)
+void test_hash_iterator(void **state)
 {
+  (void)state;
+
   uint32_t i;
-  basic_ht_t basic_hash;
   ht_iter_t ht_iterator;
   basic_key_t basic_key;
   basic_data_t basic_data;
   uint8_t key_checker[BASIC_HASH_ENTRIES_SIZE];
 
-  basic_hash = (*(basic_ht_t *)(*state));
-
   memset(key_checker, 0, sizeof(key_checker));
-  ht_iter_init(&ht_iterator, (ht_t *)&basic_hash);
+  ht_iter_init(&ht_iterator, &hash_table);
 
   while (ht_iter_get_next(&ht_iterator,
-      (ht_t *)&basic_hash, (uint8_t *)&basic_key,
+      &hash_table, (uint8_t *)&basic_key,
       (uint8_t *)&basic_data))
   {
     assert_true(key_checker[basic_key.key - 1] == 0);
@@ -148,20 +144,18 @@ void test_basic_hash_iterator(void **state)
 
 int setup(void **state)
 {
+  (void)state;
+
   uint8_t i;
   basic_key_t basic_key;
   basic_data_t basic_data;
-  basic_ht_t *basic_hash;
 
-  basic_hash = malloc(sizeof(basic_ht_t));
-
-  if (basic_hash == NULL) {
-    return (-1);
-  }
+  memset(&hash_table, 0, sizeof(hash_table));
+  memset(hash_table_data, 0, sizeof(hash_table_data));
 
   /* Initialize hash_table */
-  assert_true(ht_init((ht_t *)basic_hash, basic_hash_function,
-      BASIC_HASH_ENTRIES_SIZE, sizeof(basic_data_t), sizeof(basic_key_t)));
+  assert_true(ht_init(&hash_table, basic_hash_function, BASIC_HASH_ENTRIES_SIZE,
+      sizeof(basic_data_t), sizeof(basic_key_t), hash_table_data));
 
   /* Populate hash_table ensuring that repeated keys is not allowed */
   for (i = 1; i <= BASIC_HASH_ENTRIES_SIZE; i++)
@@ -169,22 +163,18 @@ int setup(void **state)
     basic_key.key = i;
     basic_data.x = i;
     basic_data.y = BASIC_HASH_ENTRIES_SIZE - i;
-    assert_true(ht_insert((ht_t *)basic_hash,
-        (uint8_t *)&basic_key,
+    assert_true(ht_insert(&hash_table, (uint8_t *)&basic_key,
         (uint8_t *)&basic_data));
 
     /* Check hash_table count */
-    assert_true(ht_count((ht_t *)basic_hash) == i);
+    assert_true(ht_count(&hash_table) == i);
 
-    assert_false(ht_insert((ht_t *)basic_hash,
-        (uint8_t *)&basic_key,
+    assert_false(ht_insert(&hash_table, (uint8_t *)&basic_key,
         (uint8_t *)&basic_data));
 
     /* Check hash_table count */
-    assert_true(ht_count((ht_t *)basic_hash) == i);
+    assert_true(ht_count(&hash_table) == i);
   }
-
-  *state = basic_hash;
 
   return (0);
 }
@@ -192,11 +182,23 @@ int setup(void **state)
 
 int teardown(void **state)
 {
-  basic_ht_t *basic_hash;
+  (void)state;
 
-  basic_hash = *state;
+  return (0);
+}
 
-  free(basic_hash);
+
+int group_setup(void **state)
+{
+  (void)state;
+
+  return (0);
+}
+
+
+int group_teardown(void **state)
+{
+  (void)state;
 
   return (0);
 }
@@ -206,13 +208,16 @@ int main(void)
 {
   const struct CMUnitTest tests[] =
   {
-    cmocka_unit_test(test_basic_hash),
-    cmocka_unit_test(test_basic_hash_iterator),
+    cmocka_unit_test_setup_teardown(test_hash,          setup,
+        teardown),
+    cmocka_unit_test_setup_teardown(test_hash_iterator, setup,
+        teardown),
   };
 
   cmocka_set_message_output(CM_OUTPUT_XML);
 
-  int count_fail_tests = cmocka_run_group_tests(tests, setup, teardown);
+  int count_fail_tests = cmocka_run_group_tests(tests, group_setup,
+          group_teardown);
 
   return (count_fail_tests);
 }
